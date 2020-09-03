b11ea15f6f49978dda32cf33edd7b0779cb02d93
zhoujundhr <zhoujun.mr.d@icloud.com>
2020-09-03 10:39:51 +0800

    Initial Commit

---
+++ Deary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Deary
+//
+//  Created by zhoujun on 2020/9/3.
+//  Copyright © 2020 zhoujun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Deary/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Deary
+//
+//  Created by zhoujun on 2020/9/3.
+//  Copyright © 2020 zhoujun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
